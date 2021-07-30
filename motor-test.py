8025f65b1ef3bf751d918b52d370739b0ed9c486
dartblack <dartblack48@gmail.com>
2021-07-30 19:55:27 +0400

    start

--- motor-test.py
+++ motor-test.py
@@ -1,15 +1,5 @@
 from libs.motor import Motor
 
-
-class testMotor(Motor):
-    def __init__(self):
-        Motor.__init__(self)
-        self.init_motor("main_motor")
-        self.set_debug(True)
-
-    def test(self):
-        self.control(1, 1000)
-
-
-test = testMotor()
-test.test()
+motor = Motor()
+motor.init_motor("main_motor")
+motor.control(1, 1000)
