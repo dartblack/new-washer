4618cb5b176182f3aac3a0c4ad71185711f81b8b
dartblack <dartblack48@gmail.com>
2021-07-30 20:39:00 +0400

    start

--- motor-test.py
+++ motor-test.py
@@ -3,9 +3,4 @@ from libs.motor import Motor
 main_motor = Motor()
 main_motor.init_motor("side_motor")
 main_motor.set_debug(True)
-main_motor.control(2, 7100, 0.0005)
-
-main_motor = Motor()
-main_motor.init_motor("main_motor")
-main_motor.set_debug(True)
-main_motor.control(2, 10000, 0.0005)
+main_motor.control(1, 3050, 0.0005)
