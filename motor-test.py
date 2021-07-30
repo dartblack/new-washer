63f2edcb7dd4771aa45e69c8a64ef7cd765b9b20
dartblack <dartblack48@gmail.com>
2021-07-30 20:43:18 +0400

    start

--- motor-test.py
+++ motor-test.py
@@ -3,4 +3,4 @@ from libs.motor import Motor
 main_motor = Motor()
 main_motor.init_motor("round_motor")
 main_motor.set_debug(True)
-main_motor.control(1, 1600, 0.005)
+main_motor.control(2, 1600, 0.003)
