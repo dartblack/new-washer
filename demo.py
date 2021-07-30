dc1080e5f50f7a35614a011f13309de6fe8009e3
dartblack <dartblack48@gmail.com>
2021-07-30 18:06:55 +0400

    start

---
+++ demo.py
@@ -0,0 +1,9 @@
+from libs.motor import Motor
+from libs.sensor import Sensor
+
+sensor = Sensor()
+
+sensor.init_sensors()
+print(sensor.get_left_distance())
+print(sensor.get_right_distance())
+print(sensor.lasers_info())
