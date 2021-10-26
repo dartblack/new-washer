c830aea19eacffd68768942042959fe170eb8b29
dartblack <dartblack48@gmail.com>
2021-10-26 19:16:30 +0400

    tof

--- libs/tof.py
+++ libs/tof.py
@@ -7,7 +7,7 @@ class Tof:
         self.buss = smbus2.SMBus(1)
 
     def init_sensors(self):
-        self.buss.write_block_data(self.address, 1, list([0xF5, 0x43, 0x00, 0x00, 0x00, 0x00, 0xAC, 0x45, 0x62, 0x3B]))
+        self.buss.write_block_data(self.address, 1, [0xF5, 0x43, 0x00, 0x00, 0x00, 0x00, 0xAC, 0x45, 0x62, 0x3B])
 
     def read_data(self):
         print(self.buss.read_block_data(self.address, 1))
