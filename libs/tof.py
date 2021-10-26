09b7737b669068d70a0116f2f2404d0c567f03fb
dartblack <dartblack48@gmail.com>
2021-10-26 20:48:16 +0400

    tof

--- libs/tof.py
+++ libs/tof.py
@@ -7,13 +7,8 @@ class Tof:
         self.address = 0x70
         self.buss = smbus2.SMBus(1)
 
-    def read_version(self, ):
-        readVersionPacket = list()
-        readVersionPacket.insert(0, 0x00)
-        readVersionPacket.insert(1, 0x43)
-        readVersionPacket.insert(2, [0x00, 0x00, 0x00, 0x00])
-        readVersionPacket.insert(3, [0xAC, 0x45, 0x62, 0x3B])
-        self.buss.write_block_data(self.address, 0, readVersionPacket)
+    def read_version(self):
+        self.buss.write_block_data(self.address, 0x00, [0x43, 0x00, 0x00, 0x00, 0x00, 0xAC, 0x45, 0x62, 0x3B])
         sleep(0.2)
-        print(self.buss.read_block_data(self.address, 0))
+        print(self.buss.read_block_data(self.address, 0x00))
         list().insert(0, 0x00)
