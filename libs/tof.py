760756091151be587933a57d3dc678895b938df6
dartblack <dartblack48@gmail.com>
2021-10-26 23:07:19 +0400

    tof

--- libs/tof.py
+++ libs/tof.py
@@ -8,7 +8,7 @@ class Tof:
         self.buss = SMBus(1, True)
 
     def read_version(self):
-        paket = [0xF5, 0x43, 0x00, 0x00, 0x00, 0x00, 0xAC, 0x45, 0x62, 0x3B]
-        self.buss.write_i2c_block_data(self.address, 0, paket)
+        paket = [0x43, 0x00, 0x00, 0x00, 0x00, 0xAC, 0x45, 0x62, 0x3B]
+        self.buss.write_i2c_block_data(self.address, 0xF5, paket)
         sleep(0.2)
-        print(self.buss.read_i2c_block_data(self.address, 0, 12))
+        print(self.buss.read_i2c_block_data(self.address, 0xFA, 11))
