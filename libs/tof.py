5a5aec1c6e29517809a3135f07bae781da4cf69b
dartblack <dartblack48@gmail.com>
2021-10-27 00:29:16 +0400

    tof

--- libs/tof.py
+++ libs/tof.py
@@ -7,7 +7,7 @@ class Tof:
         self.address = 0x70
         self.buss = SMBus(1, True)
 
-    def read_version(self):
+    def read_distance(self):
         paket = [0x00, 0xE0, 0x01, 0x00, 0x00, 0x00, 0x66, 0x25, 0x46, 0x93]
         data = i2c_msg.write(self.address, paket)
         read = i2c_msg.read(self.address, 12)
