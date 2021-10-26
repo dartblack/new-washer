a57fc3511a6f3ce7eac16ec005fcc04f56c2901a
dartblack <dartblack48@gmail.com>
2021-10-26 21:12:12 +0400

    tof

--- libs/tof.py
+++ libs/tof.py
@@ -9,7 +9,6 @@ class Tof:
 
     def read_version(self):
         paket = [0x00, 0x43, 0x00, 0x00, 0x00, 0x00, 0x55, 0x10, 0xCD, 0x9A]
-        for byte in paket:
-            self.buss.write_byte(self.address, byte)
+        self.buss.write_i2c_block_data(self.address, 0, paket)
         sleep(0.2)
-        print(self.buss.read_byte(self.address))
+        print(self.buss.read_i2c_block_data(self.address, 0, 16))
