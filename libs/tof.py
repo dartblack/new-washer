0a4d4dad2135fd11c3228a8d0c2be30edbcc020e
dartblack <dartblack48@gmail.com>
2021-10-26 19:10:50 +0400

    tof

--- libs/tof.py
+++ libs/tof.py
@@ -10,10 +10,4 @@ class Tof:
         self.buss.write_block_data(self.address, 1, list([0xF5, 0x43, 0x00, 0x00, 0x00, 0x00, 0xAC, 0x45, 0x62, 0x3B]))
 
     def read_data(self):
-        print(self.buss.read_byte_data(self.address, 1))
-        print(self.buss.read_byte_data(self.address, 2))
-        print(self.buss.read_byte_data(self.address, 3))
-        print(self.buss.read_byte_data(self.address, 4))
-        print(self.buss.read_byte_data(self.address, 5))
-        print(self.buss.read_byte_data(self.address, 6))
-        print(self.buss.read_byte_data(self.address, 7))
+        print(self.buss.read_block_data(self.address, 1))
