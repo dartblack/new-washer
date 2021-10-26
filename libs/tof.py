632d8c4c88b22795712e8b6d334083e17adaeff8
dartblack <dartblack48@gmail.com>
2021-10-26 23:15:04 +0400

    tof

--- libs/tof.py
+++ libs/tof.py
@@ -11,4 +11,5 @@ class Tof:
         paket = [0x43, 0x00, 0x00, 0x00, 0x00, 0xAC, 0x45, 0x62, 0x3B]
         self.buss.write_i2c_block_data(self.address, 0xF5, paket, True)
         sleep(0.2)
-        print(self.buss.read_byte(self.address))
+        for i in range(0, 20):
+            print(self.buss.read_byte(self.address))
