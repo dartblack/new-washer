26e9cd4139a6e81eb78c5a96d10212568742158f
dartblack <dartblack48@gmail.com>
2021-10-27 13:47:25 +0400

    tof

--- demo.py
+++ demo.py
@@ -1,9 +1,8 @@
-from libs.tof import Tof
+from libs.tof import TFMiniPlus
 
 # sensor = Sensor()
 # print("get distance info")
 # print(sensor.get_TFmini_data())
 
-sensor = Tof()
-sensor.read_version()
-sensor.read_distance()
+sensor = TFMiniPlus()
+sensor.get_reading_version()
