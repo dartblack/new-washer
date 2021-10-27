4f016be228cf9d98cf7a9e88a6c964b64a5a83e8
dartblack <dartblack48@gmail.com>
2021-10-27 11:03:18 +0400

    tof

--- libs/tof.py
+++ libs/tof.py
@@ -1,8 +1,7 @@
 from smbus2 import SMBus, i2c_msg
-import sys
 
 
-def calculate_crc32_byte(crc: int, dataPtr: Byte):
+def calculate_crc32_byte(crc: int, dataPtr: int):
     crc_uint32 = crc
     data_uint32 = dataPtr
     crc_uint32 = crc_uint32 ^ (data_uint32 << 24)
