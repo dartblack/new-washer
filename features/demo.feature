3df3408f52bbbd2cb0ac584eabcd8ce9f206b270
dartblack <dartblack48@gmail.com>
2021-11-09 17:12:40 +0400

    tof

--- features/demo.feature
+++ features/demo.feature
@@ -5,4 +5,12 @@ Feature: Demo
     Given Start wash car
   # Enter feature description here
   Scenario: Wash Car
-    When I Get Car Positions
+    When I Start first position
+    Then I move side motor dir "1"
+    Then I move round motor dir "1"
+    Then I move main motor dir "1"
+    Then I move round motor dir "1"
+    Then I move side motor dir "2"
+    Then I move round motor dir "1"
+    Then I move main motor dir "2"
+    Then I move side motor dir "1"
--- features/steps/demo.py
+++ features/steps/demo.py
@@ -1,16 +1,38 @@
 from behave import *
 from libs.sensor import Sensor
+from libs.motor import Motor
 
-use_step_matcher("re")
+use_step_matcher("parse")
 sensors = Sensor()
 sensors.init_sensors()
+left_distance = sensors.get_left_distance()
+right_distance = sensors.get_right_distance()
+
+main_motor = Motor()
+main_motor.init_motor("main_motor")
+
+side_motor = Motor()
+side_motor.init_motor('side_motor')
+
+round_motor = Motor()
+round_motor.init_motor('round_motor')
 
 
 @given("Start wash car")
-def step_impl(context):
+def step_start(context):
+    assert 15 < left_distance < 50
+    assert 15 < right_distance < 50
+
+
+@when("I Start first position")
+def step_start_positions(context):
     assert 1 == 1
 
 
-@when("I Get Car Positions")
-def step_impl(context):
-    assert 2 == 2
+@then('I move side motor dir "{dir}"')
+def step_move_side_motor(context, dir):
+    if dir == 1:
+        move = 300 - 70 - left_distance
+    else:
+        move = 300 - 70 - right_distance
+    side_motor.sm_control(dir, move)
