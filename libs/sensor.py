e6d98da83ac5a8b7a8f32282c9dbe4969dbedcfd
dartblack <dartblack48@gmail.com>
2021-11-13 20:39:29 +0400

    tof

--- libs/sensor.py
+++ libs/sensor.py
@@ -60,23 +60,23 @@ class Sensor(ConfigLoader):
 
     def get_back_distances(self):
         response = requests.get('http://' + self.data['DISTANCE_SENSOR_IP'] + '/distance', {
-            'count': 20,
+            'count': 30,
             'delay': 50
         })
         if response.status_code != 200:
             return 0
         data = response.json()
-        d = statistics.median_high(data['distance']) / 10
+        d = statistics.mode(data['distance']) / 10
         dis = round(math.sqrt(math.pow(d, 2) / 2))
         return dis
 
     def get_top_distances(self):
         response = requests.get('http://' + self.data['TOP_SENSOR_IP'] + '/distance', {
-            'count': 20,
+            'count': 30,
             'delay': 50
         })
         if response.status_code != 200:
             return 0
         data = response.json()
-        d = statistics.median_high(data['distance'])
+        d = statistics.mode(data['distance'])
         return d
