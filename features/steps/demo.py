4ca1e4e99294ddd2377cabba19408d114d0f8ad8
dartblack <dartblack48@gmail.com>
2021-11-18 15:03:43 +0400

    demo scenario

--- features/steps/demo.py
+++ features/steps/demo.py
@@ -78,6 +78,6 @@ def step_correct_main_motor(context, direction):
     if move > top and direction == 1:
         diff = move - top
     if direction == 2:
-        diff = main_move - move + top
+        diff = main_move - move - top
     main_motor.sm_control(direction, diff)
     assert 1 == 1
